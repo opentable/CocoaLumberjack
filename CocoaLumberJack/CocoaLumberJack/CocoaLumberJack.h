fb6dcc4074c5d49e6dbea1b61752de4317ebe9f2
Kra Larivain <olarivain@gmail.com>
2015-06-09 11:13:21 -0700

    exporting headers and all

--- CocoaLumberJack/CocoaLumberJack/CocoaLumberJack.h
+++ CocoaLumberJack/CocoaLumberJack/CocoaLumberJack.h
@@ -16,4 +16,11 @@ FOUNDATION_EXPORT const unsigned char CocoaLumberJackVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <CocoaLumberJack/PublicHeader.h>
 
-
+#import <CocoaLumberJack/DDAbstractDatabaseLogger.h>
+#import <CocoaLumberJack/DDASLLogCapture.h>
+#import <CocoaLumberJack/DDASLLogger.h>
+#import <CocoaLumberJack/DDAssert.h>
+#import <CocoaLumberJack/DDFileLogger.h>
+#import <CocoaLumberJack/DDLog.h>
+#import <CocoaLumberJack/DDLog+LOGV.h>
+#import <CocoaLumberJack/DDTTYLogger.h>
